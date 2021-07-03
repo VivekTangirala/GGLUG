a1e0704865a230166457966cf137ebb9807ac969
ruthwik56 <sairuthwik1119@gmail.com>
2021-07-03 10:50:29 +0530

    done

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gglug/Components/Screensize.dart';
-import 'package:gglug/SplashScreen.dart';
-
-import 'Loginpage/Login.dart';
+import 'package:gglug/Loginpage/Login.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -24,13 +21,12 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-<<<<<<< HEAD
   SafeArea _body() {
     return SafeArea(
       child: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
-          childern: <Widget>[
+          children: <Widget>[
             Container(
               height: 64,
               margin: EdgeInsets.only(bottom: 20),
@@ -38,83 +34,52 @@ class _MyHomePageState extends State<MyHomePage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   CircleAvatar(
-                   radius:32,
-                   backgroundImage: NetworkImage('Glug_logo.jpg'),
-                  )
-                  ,SizedBox(width: 16),
+                    radius: 32,
+                    backgroundImage: NetworkImage('Glug_logo.jpg'),
+                  ),
+                  SizedBox(width: 16),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                     Text('GLUG'),
-                     Text('69')
-                    ],
+                    children: <Widget>[Text('GLUG'), Text('69')],
                   )
                 ],
-              ),  
-            )
-            Expanded,(
-              child:GridView.builder(
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-                crossAxisCount: 2
+              ),
+            ),
+            Expanded(
+              child:GridView.count(
+                mainAxisSpacing:10,
+                crossAxisSpacing:10,
                 primary: false,
-                children: <Widget>[
+                crossAxisCount: 3,
+                children:<Widget>[
                   Card(
-                    shape: RoundedRectangleBorder(
+                    shape:RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(8)
-
-                    ),
-                    elevation: 4,
-                    child:Column(
+                    ),	
+                    elevation:4,
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget> [
-                        SvgPicture,network('Glug_logo.jpg', height:128,),
+                      children: <Widget>[
+                        
                         Text('Members')
                       ],
-                    )
-                  )
+                    ),
+                  ),
                 ]
-              )
-            )  
-          ],       
-        ),
-      ),  
-    ); 
-=======
-  Widget _body() {
-    return Container(
-      child: Center(
-        child: TextButton(
-          onPressed: () {
-            setloginfalse();
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(
-                builder: (BuildContext context) {
-                  return LoginPage();
-                },
               ),
-            );
-          },
-          child: Text("Click here to logout"),
+            ),
+          ],
         ),
       ),
     );
->>>>>>> 54adaf2321b06b93ca4f546c7dc4c490ddf02df7
   }
 
   AppBar _appBar() {
     return AppBar(
-      // elevation: 0.0,
       leading: IconButton(
-        icon: Icon(
-          Icons.notifications,
-        ),
-        onPressed: () {
-          print("bell");
-          getProportionateScreenWidth(100.0);
-          getProportionateScreenHeight(100.0);
-        },
+        icon: Icon(Icons.notifications),
+        onPressed: () {},
       ),
       title: Center(
         child: Text("GITAM-GLUG"),
